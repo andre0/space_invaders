e66cbb81a6de6b7f17020229873760c228df51ee
Andre Le <andrele2512@gmail.com>
2019-02-13 01:34:22 -0800

    player movement

--- space_invaders/Assets/Scripts/Player.cs
+++ space_invaders/Assets/Scripts/Player.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    private Collider2D playerCollider;
+    private Rigidbody2D playerRigidbody;
+    private float speed = 6f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
         
+    void Move()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        Vector2 currentVelocity = playerRigidbody.velocity;
+        playerRigidbody.velocity = new Vector2(movementX * speed, currentVelocity.y);
     }
 }
