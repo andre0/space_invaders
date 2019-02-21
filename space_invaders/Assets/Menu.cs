4bb2e5306222c78cca66d7a608a0261f82411c3e
andre0 <andrele2512@gmail.com>
2019-02-20 18:51:36 -0800

    Start Menu

---
+++ space_invaders/Assets/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("level");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
